a5cf342551dca2cb480b6f918814094a29aded46
BSP/IOD <innovativeopsdiv.bsp@protonmail.com>
2026-04-30 23:47:32 +0200

    Create h2s.py

---
+++ h2s.py
@@ -0,0 +1,10 @@
+print("""
+                Clukk
+             Release V2.3
+             
+                 H2S
+""")
+print("")
+hours = float(input("Enter amount of hours:"))
+seconds = hours * 60 * 60
+print("Hours converted to seconds:", seconds)
